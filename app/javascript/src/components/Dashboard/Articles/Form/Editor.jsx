6a1c1661b38c84e8b9d3593316478651829def4b
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-02 12:09:37 +0530

    Added create article UI

---
+++ app/javascript/src/components/Dashboard/Articles/Form/Editor.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { FormikEditor } from "@bigbinary/neeto-editor";
+import { ErrorMessage, useFormikContext } from "formik";
+import { Textarea } from "neetoui/formik";
+import { useTranslation } from "react-i18next";
+
+const Editor = ({ editorRef }) => {
+  const { t } = useTranslation();
+
+  const { setFieldValue } = useFormikContext();
+
+  return (
+    <FormikEditor
+      required
+      className="space-y-3 "
+      contentClassName="editor-content"
+      menuType="fixed"
+      placeholder={t("placeholder.addDescription")}
+      ref={editorRef}
+      onChange={content => setFieldValue("description", content)}
+    >
+      <div className="editor-title space-y-10">
+        <Textarea
+          nakedTextarea
+          name="title"
+          placeholder={t("placeholder.articleTitle")}
+          rows={1}
+        />
+        <ErrorMessage
+          className="mt-5 text-xs text-red-600"
+          component="p"
+          name="description"
+        />
+      </div>
+    </FormikEditor>
+  );
+};
+
+export default Editor;
