1e459be60d8fe67779525518667cec03cdbd5d57
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-03 22:35:27 +0530

    Added destroy, show and delete actions

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Api::V1::ArticlesController < ApplicationController
+  before_action :load_article!, only: %i[show update destroy]
+
   def index
-    @articles = Article.all
+    @articles = Article.includes([:author, :category]).all
   end
 
   def create
@@ -10,9 +12,27 @@ class Api::V1::ArticlesController < ApplicationController
     render_notice(t("successfully_created", entity: "Article"))
   end
 
+  def show
+    render
+  end
+
+  def update
+    @article.update!(article_params)
+    render_notice(t("successfully_updated", entity: "Article"))
+  end
+
+  def destroy
+    @article.destroy!
+    render_notice(t("successfully_deleted", entity: "Article"))
+  end
+
   private
 
     def article_params
       params.require(:article).permit(:title, :description, :status, :category_id)
+    end
+
+    def load_article!
+      @article = Article.find_by!(slug: params[:slug])
     end
 end
