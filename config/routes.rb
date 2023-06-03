d8886b1f3de7f6acb08b0cca1c2966c2a84e035b
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-03 22:36:36 +0530

    Added controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         resources :categories, only: %i[index create]
-        resources :articles, only: %i[index create]
+        resources :articles, except: %i[edit new], param: :slug
       end
     end
   end
