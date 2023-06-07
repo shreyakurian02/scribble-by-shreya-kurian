6c2bf12b10dc4d2da83e22828d4bcb23c48b226c
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-07 21:55:13 +0530

    Added sites controller tests

--- app/javascript/src/components/Dashboard/Settings/General/constants.js
+++ app/javascript/src/components/Dashboard/Settings/General/constants.js
@@ -6,5 +6,6 @@ export const INITIAL_VALUES = { siteTitle: "" };
 export const VALIDATION_SCHEMA = yup.object().shape({
   siteTitle: yup
     .string()
+    .matches(/.*[a-zA-Z0-9].*/, t("validations.validAlphaNumeric"))
     .required(t("validations.required", { entity: t("common.siteTitle") })),
 });
---
+++ test/controllers/api/v1/sites_controller_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Api::V1::SitesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @site = create :site
+  end
+
+  def test_should_update_category
+    new_title = "Updated title"
+    put(api_v1_site_path, params: { site: { title: new_title } }, headers:)
+    assert_response :success
+    assert_equal I18n.t("successfully_updated", entity: "Site"), response_json["notice"]
+    assert_equal new_title, @site.reload.title
+  end
+
+  def test_shouldnt_update_site_with_invalid_title
+    assert_no_difference "Site.count" do
+      put(api_v1_site_path, params: { site: { title: "" } }, headers:)
+      assert_response :unprocessable_entity
+    end
+  end
+
+  def test_should_show_site
+    get(api_v1_site_path, headers:)
+    assert_response :success
+  end
+end
