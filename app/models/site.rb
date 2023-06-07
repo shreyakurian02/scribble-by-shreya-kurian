2ae04d9bd1c7046431d16ec26b1445e4a9dddc61
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-07 21:34:02 +0530

    Added site model tests

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Site < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true,
+    format: { with: Constants::ALPHANUMERIC_FORMAT_REGEX, message: I18n.t("errors.alphanumeric") }
 end
