15ce7fc621e120c53afbc9bd213f63953f1d6810
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-08 23:20:16 +0530

    Added custom validations

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -5,18 +5,41 @@ class Redirection < ApplicationRecord
 
   validates :from_path, presence: true, uniqueness: { scope: :site_id }
   validates :to_path, presence: true
-  validate :is_redirection_valid?
+  validate :is_redirection_valid
 
   private
 
-    def is_redirection_valid?
-      errors.add(:to_path, I18n.t("errors.same_path")) if to_path == from_path
+    def is_redirection_valid
+      byebug
+      errors.add(
+        :to_path,
+        I18n.t("errors.same_path")) if urls_equal? || paths_equal?
 
-      if site.redirections.exists? to_path, from_path
+      if site.redirections.exists? from_path: to_path, to_path: from_path
         errors.add(:to_path, I18n.t("errors.loop"))
       else
         errors.add(:to_path, I18n.t("errors.chain_path", path: "from")) if site.redirections.exists? from_path: to_path
-        errors.add(:from_path, I18n.t("errors.chain_path", path: "to")) if site.redirection.exists? to_path: from_path
+        errors.add(:from_path, I18n.t("errors.chain_path", path: "to")) if site.redirections.exists? to_path: from_path
       end
+    end
+
+    def urls_equal?
+      to_path_host_same_as_app_host? && paths_equal?
+    end
+
+    def paths_equal?
+      parsed_to_path.path == from_path
+    end
+
+    def to_path_host_same_as_app_host?
+      to_path_host == Rails.application.secrets[:host]
+    end
+
+    def parsed_to_path
+      @_parsed_to_path ||= URI.parse(to_path)
+    end
+
+    def to_path_host
+      parsed_to_path.host
     end
 end
