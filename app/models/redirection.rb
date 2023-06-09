d122a1ece0f060004a979b84710b3462b16b2dbb
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-09 09:13:46 +0530

    Added redirection model tests

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -5,15 +5,12 @@ class Redirection < ApplicationRecord
 
   validates :from_path, presence: true, uniqueness: { scope: :site_id }
   validates :to_path, presence: true
-  validate :is_redirection_valid
+  validate :validate_redirection
 
   private
 
-    def is_redirection_valid
-      byebug
-      errors.add(
-        :to_path,
-        I18n.t("errors.same_path")) if urls_equal? || paths_equal?
+    def validate_redirection
+      errors.add(:to_path, I18n.t("errors.same_path")) if urls_equal? || paths_equal_if_host_nil?
 
       if site.redirections.exists? from_path: to_path, to_path: from_path
         errors.add(:to_path, I18n.t("errors.loop"))
@@ -25,6 +22,10 @@ class Redirection < ApplicationRecord
 
     def urls_equal?
       to_path_host_same_as_app_host? && paths_equal?
+    end
+
+    def paths_equal_if_host_nil?
+      to_path_host.nil? && paths_equal?
     end
 
     def paths_equal?
