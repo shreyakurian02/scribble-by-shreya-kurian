61899756911c9b9bc420a300d038f0eaa897e1dd
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-16 13:43:41 +0530

    Modified categories reordering

--- app/javascript/src/apis/categories.js
+++ app/javascript/src/apis/categories.js
@@ -7,15 +7,17 @@ const fetch = params => axios.get(CATEGORIES_BASE_URL, { params });
 const create = payload =>
   axios.post(CATEGORIES_BASE_URL, { category: payload });
 
-const update = ({ id, payload }) =>
-  axios.put(`${CATEGORIES_BASE_URL}/${id}`, payload);
+const update = ({ id, payload, quiet = false }) => {
+  const path = quiet
+    ? `${CATEGORIES_BASE_URL}/${id}?quiet`
+    : `${CATEGORIES_BASE_URL}/${id}`;
+
+  return axios.put(path, payload);
+};
 
 const destroy = ({ id, params }) =>
   axios.delete(`${CATEGORIES_BASE_URL}/${id}`, { params });
 
-const reorder = ({ id, payload }) =>
-  axios.patch(`${CATEGORIES_BASE_URL}/${id}/reorder`, { category: payload });
-
-const categoriesApi = { fetch, create, reorder, destroy, update };
+const categoriesApi = { fetch, create, destroy, update };
 
 export default categoriesApi;
