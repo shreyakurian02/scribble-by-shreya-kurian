e339153ca58626e6fd2ec2344cd351c5e0a4be19
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-22 11:03:04 +0530

    Fixed categories test

--- test/controllers/api/v1/public/categories_controller_test.rb
+++ test/controllers/api/v1/public/categories_controller_test.rb
@@ -6,12 +6,20 @@ class Api::V1::Public::CategoriesControllerTest < ActionDispatch::IntegrationTes
   def setup
     @site = create :site
     @headers = public_headers(@site)
-    create_list :category, 5, site: @site
+    @categories= create_list :category, 5, site: @site
   end
 
-  def test_should_list_all_categories
-    get(api_v1_public_categories_path, headers: @headers)
+  def test_should_only_list_categories_with_published_articles
+    create(:article, :published, category: @categories.first)
+    categories_with_published_articles = @site.categories.joins(:articles).where(articles: { status: Article.statuses[:published] }).distinct
+    get api_v1_public_categories_path, headers: @headers
     assert_response :success
-    assert_equal @site.categories.length, response_json["categories"].length
+    assert_equal categories_with_published_articles.length, response_json["categories"].length
+  end
+
+  def test_categories_is_not_accessable_to_unauthenticated_user
+    get(api_v1_public_categories_path, headers:)
+    assert_response :unauthorized
+    assert_includes response_json["error"], I18n.t("session.could_not_auth")
   end
 end
