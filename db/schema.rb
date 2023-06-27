ce2362501d811e43b92b2326b12d27fd09143ba9
Shreya Kurian <shreyakurian99@gmail.com>
2023-06-27 10:45:27 +0530

    Added views column to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_052857) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_070253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_052857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id", null: false
+    t.integer "views", default: 0, null: false
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
     t.index ["user_id"], name: "index_articles_on_user_id"
