b03d3c00070396827321b86762c9564737ad219a
Shreya Kurian <shreyakurian99@gmail.com>
2023-07-08 09:13:55 +0530

    Added tests for pdf generation

---
+++ test/controllers/api/v1/articles/reports_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Api::V1::Articles::ReportsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @site = create :site
+    @user = create :user, site: @site
+  end
+
+  def test_can_generate_report
+    post(api_v1_report_path, headers:)
+    assert_response :success
+    assert @user.reload.report.attached?
+  end
+
+  def test_can_download_report
+    report = ActiveStorage::Blob.create_and_upload!(
+      io: File.open("test/fixtures/report.pdf"),
+      filename: "articles-report.pdf",
+      content_type: "application/pdf"
+    )
+    @user.report.attach(report)
+    get(download_api_v1_report_path, headers:)
+    assert_response :success
+  end
+
+  def test_download_report_failure
+    get(download_api_v1_report_path, headers:)
+    assert_response :not_found
+    assert_includes response_json["error"], "Report not found"
+  end
+end
